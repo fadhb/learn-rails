0f3806b14155543d169c8b788459f346583e83a2
Jonathan Headen <jonathan.headen@outlook.com>
2014-04-07 16:25:05 +1000

    changed git global push setting to simple

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+
   has_no_table
   column :name,:string
   column :email, :string
@@ -10,4 +11,5 @@ class Contact < ActiveRecord::Base
   validates_format_of :email,
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
+
 end
